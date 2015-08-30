192f4b11a3a205025e604ac4aed6cb4a1cb478cf
amarjeetkapoor1 <amarjeet.kapoor1@gmail.com>
2015-08-30 21:44:38 +0530

    poll app start from first address

--- polls/views.py
+++ polls/views.py
@@ -40,7 +40,7 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('results', args=(p.id,)))
 
 
 def results(request, question_id):
